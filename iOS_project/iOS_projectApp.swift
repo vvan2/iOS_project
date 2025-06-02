656963704298e0e43fd434aec665b7c86346a25f
Son Juwan <wndhks171@naver.com>
2025-06-02 17:07:06 +0900

    Initial Commit

---
+++ iOS_project/iOS_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_projectApp.swift
+//  iOS_project
+//
+//  Created by sonjuwan on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
